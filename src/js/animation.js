3c4d0a03e6a51c2b6ec7d6e90954a85ced3fcd20
pgm-arnadebu <arnaud.deburchgraeve@student.arteveldehs.be>
2022-06-05 23:39:48 +0200

    05/06

---
+++ src/js/animation.js
@@ -0,0 +1,16 @@
+import gsap from 'gsap';
+
+export const animations = {
+  init() {
+    this.generateUI();
+  },
+  generateUI() {
+    this.generateHeader();
+
+  },
+  generateHeader() {
+
+  }
+};
+
+animations.init();
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
 import { logSomething } from './module';
+import { animations } from './animation';
 
 logSomething('Hello World');
+animations();
